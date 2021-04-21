9885ae962ef78f816629dcd859c2e59c75ae9ec0
profeblas <profeblas@localhost>
2021-04-21 16:56:37 -0300

    Comenzando la sopa

--- POO21_TP1_E14/src/poo21_tp1_e14/POO21_TP1_E14.java
+++ POO21_TP1_E14/src/poo21_tp1_e14/POO21_TP1_E14.java
@@ -5,18 +5,69 @@
  */
 package poo21_tp1_e14;
 
+import java.util.Arrays;
+
 /**
  *
  * @author blaslopez@gmail.com
  */
 public class POO21_TP1_E14 {
+    
+    private static char sopa[][];
+    
+    private static char caracterAleatorio(){
+        // 65 ≤ A[i, j] ≤ 90 (Código ASCCII)
+        return ' ';
+    }
+    
+    private static char obtener(int f, int c){
+        return sopa[f][c];
+    }
+    
+    private static void establecer(int f, int c, char car){
+        sopa[f][c] = car;
+    }
+    
+    private static boolean coordenadaValida(int f, int c){
+        return ( (f<=8)&&(c<=10) );
+    }
+    
+    private static boolean buscarNorte(String p,int f, int c){
+        //...
+    }
+    private static boolean buscarPalabra(String p,int f, int c){
+        return ((coordenadaValida(f,c)&&( buscarNorte(p,f,c) || buscarSur(p,f,c) ...     )));
+    }
+            
+    private static char[][] crearSopa(int m, int n){
+        char s[][];
+        int f,c;
+        
+        s = new char[m][n];
+        for(f=0;f<m;f++)
+            for(c=0;c<n;c++){
+                s[f][c] = caracterAleatorio();
+            }
+        
+        return s;
+    }
+    
+    private static char[] devolver(int n){
+        char res[];
+        res = new char[n];
+        Arrays.fill(res,' ');
+        return res;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola git.");
+        System.out.println("Sopa de letras");
+        sopa = crearSopa(8,10);
+       
+        
     }
     
 }
